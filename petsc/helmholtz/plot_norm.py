868c29f3d80bfc41fe10f1b0d40864c1649cb27c
danfinn <dannyfinn10@gmail.com>
2021-04-01 22:42:16 -0400

    Adding implementation to READme and literature review latex docs

--- petsc/helmholtz/plot_norm.py
+++ petsc/helmholtz/plot_norm.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 cgIN = open('cg_sor.txt' , 'r')
-richardsonIN = open('richardson.txt', 'r')
+richardsonIN = open('richardson_sor.txt', 'r')
 
 
 cgIt = []; cgNorm = []
@@ -10,21 +10,21 @@ cgIt = []; cgNorm = []
 for line in cgIN.readlines():
 
     cgIt.append( float( line.split()[0]))
-    cgNorm.append( float( line.split()[3] ) )
+    cgNorm.append( float( line.split()[4] ) )
 
 richIt = []; richNorm = []
 
-for line in richarsonIN.readlines():
+for line in richardsonIN.readlines():
 
     richIt.append( float( line.split()[0] ) )
-    richNorm.append( float( line.split()[1] ) )
+    richNorm.append( float( line.split()[4] ) )
 
 
 plt.clf()
 
 plt.plot( cgIt , cgNorm , 'r-', label='Conjugate Gradients')
 plt.plot( richIt, richNorm, 'k-', label='Richarson')
-plt.xlabel( 'Iteration' ), plt.ylabel( 'Norm' )
+plt.xlabel( 'Iteration' ), plt.ylabel( 'Norm Residual' )
 plt.yscale('log')
 plt.legend( loc='upper right' , numpoints = 1 )
 plt.title( 'Residual Convergence Comparison')
